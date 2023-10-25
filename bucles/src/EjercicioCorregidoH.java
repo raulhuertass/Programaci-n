a3d829ad375db6a5fc430ea45ba7b4c122943ea1
Raúl Huertas Camarena <raulhc44@gmail.com>
2023-10-25 10:03:32 +0200

    Raul

--- bucles/src/EjercicioCorregidoH.java
+++ bucles/src/EjercicioCorregidoH.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class EjercicioCorregidoH {
     public static void main(String[] args) {
 
+        String respuesta;
+        int opcion=1, iRojo=0, iVerde=0, iAmarillo=0, iNegro=0 ;
+
+        Scanner sc= new Scanner(System.in);
+        
+        while (opcion==1 || opcion==2){
+            
+            System.out.println("Menú");
+            respuesta=sc.nextLine();
+            opcion=Integer.valueOf(respuesta);
+            
+            if (opcion==1){
+                System.out.println("¿El paciente camina. si/no?");
+                respuesta=sc.nextLine();
+                
+                    if (respuesta.compareTo("si")==0){
+
+                    }
+                        
+            } else if (opcion==2) {
+                
+            }
+            {
+                
+            }
+
+
+        }
+        
+        
+        
+        
     }
 }
